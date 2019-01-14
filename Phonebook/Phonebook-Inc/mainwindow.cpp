735b268886c8c54aa7e1990b6a4bd02b4592612e
HWerneck <werneck.henrique@outlook.com>
2019-01-14 14:27:07 -0200

    Update mainwindow.cpp

--- Phonebook/Phonebook-Inc/mainwindow.cpp
+++ Phonebook/Phonebook-Inc/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <QtGui>
 #include <QWidget>
+#include <QLabel>
 #include <QLineEdit>
 #include <QComboBox>
 #include <QPushButton>
@@ -13,26 +14,60 @@ MainWindow::MainWindow(QWidget *parent)
 {
     widget = new QWidget();
     
+    name_label = new QLabel(tr("Name:"));
     name_line = new QLineEdit();
+    email_label = new QLabel(tr("Email:"));
     email_line = new QLineEdit();
+    phone_label = new QLabel(tr("Phone:"));
     phone_line = new QLineEdit();
     phone_type_combobox = new QComboBox();
     
-    phone_type_combobox.setEditable(false);
-    
+    phone_type_combobox->setEditable(false);
+    phone_type_combobox->addItem(tr("Home"));
+    phone_type_combobox->addItem(tr("Cell"));
     
     edit_button = new QPushButton(tr("E"));
     save_button = new QPushButton(tr("S"));
     cancel_button = new QPushButton(tr("C"));
+    save_button->hide();
+    cancel_button->hide();
 
-    QHBoxLayout *layoutUp = new QHBoxLayout;
-    layoutUp->addStretch();
-    layoutUp->addWidget(Pokemon_add_button);
-    layoutUp->addStretch();
+    QHBoxLayout *layoutName = new QHBoxLayout;
+    layoutName->addWidget(name_label);
+    layoutName->addWidget(name_line);
+    
+    QHBoxLayout *layoutEmail = new QHBoxLayout;
+    layoutEmail->addWidget(email_label);
+    layoutEmail->addWidget(email_line);
+    
+    QVBoxLayout *layoutNE = new QVBoxLayout;
+    layoutNE->addLayout(layoutName);
+    layoutNE->addLayout(layoutEmail);
+    
+    QHBoxLayout *layoutButton = new QHBoxLayout;
+    layoutButton->addStretch();
+    layoutButton->addWidget(edit_button);
+    layoutButton->addWidget(save_button);
+    layoutButton->addWidget(cancel_button);
+    
+    QVBoxlayout *layoutButtons = new QVBoxLayout;
+    layoutButtons->addLayout(layoutButton);
+    layoutButtons->addStretch();
+    
+    QHBoxLayout *layoutNEButtons = new QHBoxLayout;
+    layoutNEButtons->addLayout(layoutNE);
+    layoutNEButtons->addLayout(layoutButtons);
+    
+    QHBoxLayout *layoutPhone = new QHBoxLayout;
+    layoutPhone->addStretch();
+    layoutPhone->addWidget(phone_label);
+    layoutPhone->addWidget(phone_line);
+    layoutPhone->addWidget(phone_type_combobox);
+    layoutPhone->addStretch();
 
     QVBoxLayout *layout = new QVBoxLayout;
-    layout->addLayout(layoutUp);
-    layout->addWidget(Pokemon_save_button);
+    layout->addLayout(layoutNEButtons);
+    layout->addLayout(layoutPhone);
 
     connect(Pokemon_add_button, SIGNAL(clicked()), this, SLOT(addPokemon()));
     connect(Pokemon_edit_button, SIGNAL(toggled(bool)), Pokemon_save_button, SLOT(setVisible(bool)));
