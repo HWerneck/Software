76f33940adeec1e028d4788382e0ac9c646c5f12
HWerneck <werneck.henrique@outlook.com>
2019-01-15 14:46:01 -0200

    Update animal.cpp

--- Animal List/animal.cpp
+++ Animal List/animal.cpp
@@ -2,8 +2,18 @@
 
 class Animal
 {
+private:
     int id;
     std::string name;
     std::string species;
     int paw_number;
+
+public:
+    Animal()
+    {
+        id = 0;
+        name = "";
+        species = "";
+        paw_number = 0;
+    }
 };
