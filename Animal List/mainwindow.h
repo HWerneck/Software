219288692c364059330e8024bbb8c463e653dc93
HWerneck <werneck.henrique@outlook.com>
2019-01-15 21:12:17 -0200

    Update mainwindow.h

--- Animal List/mainwindow.h
+++ Animal List/mainwindow.h
@@ -8,7 +8,8 @@ class QListWidget;
 class QPushButton;
 class QLineEdit;
 class QGridLayout;
-class QHBoxWidget;
+class QHBoxLayout;
+class QVBoxLayout;
 
 class MainWindow : public QMainWindow
 {
@@ -18,16 +19,34 @@ public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void editButton_clicked();
+    void saveButton_clicked();
+    void cancelButton_clicked();
+
 private:
     QWidget *widget;
+
+    QPushButton *add_button;
+    QPushButton *delete_button;
     QListWidget *list;
-    
+
     QLineEdit *id_line;
     QLineEdit *name_line;
     QLineEdit *species_line;
     QLineEdit *paw_line;
-    
-    QGridLayout *layoutRight;
+
+    QPushButton *edit_button;
+    QPushButton *save_button;
+    QPushButton *cancel_button;
+
+    QHBoxLayout *layoutLeft1;
+    QVBoxLayout *layoutLeft;
+
+    QHBoxLayout *layoutRight1;
+    QHBoxLayout *layoutRight2;
+    QHBoxLayout *layoutRight3;
+    QVBoxLayout *layoutRight;
     QHBoxLayout *layout;
 };
 
