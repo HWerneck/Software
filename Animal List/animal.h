e68edc16acff95c6d408af4b056276b5a2a11aea
HWerneck <werneck.henrique@outlook.com>
2019-01-20 23:37:55 -0200

    Update animal.h

--- Animal List/animal.h
+++ Animal List/animal.h
@@ -16,6 +16,14 @@ class Animal
 public:
     Animal(int id);
     ~Animal();
+    int getID();
+    void setID(int animal_id);
+    QString getName();
+    void setName(QString animal_name);
+    QString getSpecies();
+    void setSpecies(QString animal_species);
+    int getType();
+    void setType(int animal_type);
 
 private:
     int id;
