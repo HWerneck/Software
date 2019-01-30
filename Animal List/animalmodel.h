19ab19d8e8c93fbf75a7e32bb965af3c470675b6
HWerneck <werneck.henrique@outlook.com>
2019-01-30 11:28:33 -0200

    Create animalmodel.h

---
+++ Animal List/animalmodel.h
@@ -0,0 +1,15 @@
+class AnimalModel : public QAbstractTableModel
+{
+	Q_OBJECT
+	
+public:
+	AnimalModel(QObject *parent = 0);
+	int rowCount(const QModelIndex &parent) const;
+	int columnCount(const QModelIndex &parent) const;
+	QVariant data(const QModelIndex &parent, int role) const;
+	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
+private:
+	QStringList animal_names;
+	QVector<QIcon> animal_types;
+};
