edc8fb5e6bda84ba1819d84d60595f2ca806970f
HWerneck <werneck.henrique@outlook.com>
2019-01-30 11:53:16 -0200

    Create animalmodel.cpp

---
+++ Animal List/animalmodel.cpp
@@ -0,0 +1,47 @@
+#include "animalmodel.h"
+
+AnimalModel::AnimalModel(QObject *parent) :
+	QAbstractTableModel(parent)
+{
+}
+
+int AnimalModel::rowCount(const QModelIndex & /* parent */) const
+{
+	return animal_names.count();
+}
+
+int AnimalModel::columnCount(const QModelIndex & /* parent */) const
+{
+	return 2;
+}
+
+QVariant AnimalModel::data(const QModelIndex &index, int role) const
+{
+	if (!index.isValid())
+	{
+		return QVariant();
+	}
+	
+	if (role == Qt::DisplayRole)
+	{
+		if (index.column == 0)
+		{
+			int id = index.row() + 1;
+			QString animal_id = QString::number(id);
+			return animal_id;
+		}
+		else if (index.column == 1)
+		{
+			return animal_names.at(index.row());
+		}
+	}
+	else if (role == Qt::DecorationRole)
+	{
+		return QIcon animal_types.at(index.row());
+	}
+	return QVariant();
+}
+
+QVariant AnimalModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+}
