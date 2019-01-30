01b728d975f8628a376b4e411d8f7d954c042e4e
HWerneck <werneck.henrique@outlook.com>
2019-01-30 11:35:59 -0200

    Update animalmodel.h

--- Animal List/animalmodel.h
+++ Animal List/animalmodel.h
@@ -6,7 +6,7 @@ public:
 	AnimalModel(QObject *parent = 0);
 	int rowCount(const QModelIndex &parent) const;
 	int columnCount(const QModelIndex &parent) const;
-	QVariant data(const QModelIndex &parent, int role) const;
+	QVariant data(const QModelIndex &index, int role) const;
 	QVariant headerData(int section, Qt::Orientation orientation, int role) const;
 
 private:
